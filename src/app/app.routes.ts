9e13686e820f1f6dd7d519a953c3a4f052996339
Toqa Youness <toqamohamedabdulaziz44@gmail.com>
2026-06-28 21:59:38 +0300

    routes and app component edits

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,45 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './features/auth/pages/register/register.component';
+import { LoginComponent } from './features/auth/pages/login/login.component';
+import { ProjectComponent } from './features/project/pages/project/project/project.component';
+import { AuthLayoutComponent } from './layout/auth-layout/auth-layout/auth-layout.component';
+import { MainLayoutComponent } from './layout/main-layout/main-layout/main-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'register',
+    pathMatch: 'full',
+  },
+  {
+    path: '',
+    component: AuthLayoutComponent,
+    children: [
+      {
+        path: 'register',
+        component: RegisterComponent,
+        title: 'Register',
+      },
+      {
+        path: 'login',
+        component: LoginComponent,
+        title: 'Login',
+      },
+    ],
+  },
+  {
+    path:'',
+    component:MainLayoutComponent,
+    children:[
+{
+    path: 'project',
+    component: ProjectComponent,
+    title: 'Project',
+  }
+    ]
+  },
+  {
+    path: '**',
+    redirectTo: 'register',
+  },
+];
